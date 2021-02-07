29d0b3b31677d3ecd5c53983658e0cc84a0a881b
Jens Henneberg <jens@henneberg.dk>
2021-02-07 12:15:57 +0100

    * Renames * Unit test added

--- test/RulesEngine.test/RulesEngineUnitTest.cs
+++ test/RulesEngine.test/RulesEngineUnitTest.cs
@@ -27,8 +27,29 @@ namespace RulesEngine.test
             var businessRulesEngine = provider.GetService<IBusinessRulesEngine>();
             businessRulesEngine.Run(order);
             testRule.Verify(x => x.Applies(order));
-            if (apply)
-                testRule.Verify(x => x.Apply(order));
+            testRule.Verify(x => x.Apply(order), apply ? Times.Once : Times.Never);
+        }
+
+        [Fact]
+        public void TestThatMultipleBusinessRulesAreApplied()
+        {
+            var order = new Order();
+            var applicableRule = new Mock<IBusinessRule>();
+            applicableRule.Setup(x => x.Applies(It.IsAny<Order>())).Returns(true);
+            services.AddSingleton<IBusinessRule>(applicableRule.Object);
+
+            var nonApplicableRule = new Mock<IBusinessRule>();
+            nonApplicableRule.Setup(x => x.Applies(It.IsAny<Order>())).Returns(false);
+            services.AddSingleton<IBusinessRule>(nonApplicableRule.Object);
+
+            var provider = services.BuildServiceProvider();
+            var businessRulesEngine = provider.GetService<IBusinessRulesEngine>();
+            businessRulesEngine.Run(order);
+            applicableRule.Verify(x => x.Applies(order), Times.Once);
+            applicableRule.Verify(x => x.Apply(order), Times.Once);
+            nonApplicableRule.Verify(x => x.Applies(order), Times.Once);
+            nonApplicableRule.Verify(x => x.Apply(order), Times.Never);
+
         }
     }
 }
